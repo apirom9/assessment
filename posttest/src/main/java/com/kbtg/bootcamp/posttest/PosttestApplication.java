3fe1426327c7a97cf8c479b23b21c9725e2b39d7
Apirom Na Nakorn <apirom9@gmail.com>
2024-02-24 12:42:35 +0700

    Add retry connect db on start up

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/PosttestApplication.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/PosttestApplication.java
@@ -1,7 +1,14 @@
 package com.kbtg.bootcamp.posttest;
 
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.support.DatabaseStartupValidator;
+
+import javax.sql.DataSource;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class PosttestApplication {
@@ -10,4 +17,22 @@ public class PosttestApplication {
 		SpringApplication.run(PosttestApplication.class, args);
 	}
 
+	@Bean
+	public DatabaseStartupValidator databaseStartupValidator(DataSource datasource){
+		DatabaseStartupValidator databaseStartupValidator = new DatabaseStartupValidator();
+		databaseStartupValidator.setDataSource(datasource);
+		databaseStartupValidator.setInterval(10);
+		databaseStartupValidator.setTimeout(120);
+		return databaseStartupValidator;
+	}
+
+	@Bean
+	public static BeanFactoryPostProcessor dependsOnPostProcessor() {
+		return bf -> {
+			String[] jpa = bf.getBeanNamesForType(EntityManagerFactory.class);
+			Stream.of(jpa)
+					.map(bf::getBeanDefinition)
+					.forEach(it -> it.setDependsOn("databaseStartupValidator"));
+		};
+	}
 }
